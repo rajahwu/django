f336a7c2999644fbff66b6e499782180be53781e
rajahwu <vmradford@gmail.com>
2023-10-13 23:59:54 -0400

    complete course

--- project/notes/views.py
+++ project/notes/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+    
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    form_class = NotesForm
+    
 
 class NotesCreateView(CreateView):
     model = Notes
     form_class = NotesForm
     success_url = '/smart/notes'
+    login_url = "/admin"
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    login_url = "/admin"
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
