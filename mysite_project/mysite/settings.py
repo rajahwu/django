a2ccdb1ed0feabedf0ab26407d65375958b5c1e0
rajahwu <vmradford@gmail.com>
2023-10-15 09:19:12 -0400

    add static stylesheets

--- mysite_project/mysite/settings.py
+++ mysite_project/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
