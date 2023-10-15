c16fa7d89b9dc8dd01de5ef25e9ce9bfce2f2ea7
rajahwu <vmradford@gmail.com>
2023-10-14 20:16:47 -0400

    render question and choices

--- mysite_project/polls/views.py
+++ mysite_project/polls/views.py
@@ -10,7 +10,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"quesiton": question})
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question $s" % question_id)
